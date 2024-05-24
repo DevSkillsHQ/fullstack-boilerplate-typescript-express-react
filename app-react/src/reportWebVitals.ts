c7068fcf4a668ffdcc82a8b8464afb5a480b22c0
Konstantin Sharov <kostsh7@gmail.com>
2024-05-24 14:48:29 +0200

    update web-vitals

--- app-react/src/reportWebVitals.ts
+++ app-react/src/reportWebVitals.ts
@@ -1,15 +1,15 @@
-import { ReportHandler } from 'web-vitals';
+import { onCLS, onINP, onLCP, onFCP, onTTFB, Metric } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+type VitalReportHandler = (metric: Metric) => void;
+
+const reportWebVitals = (onPerfEntry?: VitalReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+      onCLS(onPerfEntry);
+      onFCP(onPerfEntry);
+      onINP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
   }
-};
+}
 
 export default reportWebVitals;
